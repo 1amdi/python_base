3c8da7d4d795abd30a65d757b5b64cc9a6ed8df9
jiaogong <gongjiao@oneniceapp.com>
2019-10-17 22:08:51 +0800

    chapter2.2

--- README.md
+++ README.md
@@ -1,12 +1,28 @@
-# python_base
-python3 basic grammer practice
+# python3 basic grammer practice
 
 #chapter 2
 ##2.1
-容器序列（可存放不同类型数据）:
-list,tuple,collections.deque
-扁平序列（一种类型）:
-str,bytes,bytearray,memoryview,array,array
+- 容器序列（可存放不同类型数据)
+> list,tuple,collections.deque
+- 扁平序列（一种类型）:
+> str,bytes,bytearray,memoryview,array,array
+
+##2.2
+- 列表推导
+> 构建列表（list）的快捷方式
+- 生成器
+> 遵守迭代器协议，逐个产出元素，节省内存
+##代码引用
+`hello world`
+
+```
+line1
+line2
+```
+链接和图片
+[百度](http://www.baidu.com)
+![](https://pskoty.oss-cn-beijing.aliyuncs.com/post/learn-markdown/7.png)
+
 
 
 
--- chapter2/list_gene.py
+++ chapter2/list_gene.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#example 2.1 字符串转unicode码位列表 for循环
+#example 2-1 字符串转unicode码位列表 for循环
 
 symbols ='$¥€'
 codes = []
@@ -8,7 +8,18 @@ for symbol in symbols:
     codes.append(ord(symbol))
 print(codes)
 
+#example 2-2 列表推导式
 codes = [ord(symbol) for symbol in symbols]
 print(codes)
 
+
+#example 2-3 列表推导式条件判断
+beyond_ascii = [ord(s) for s in symbols if ord(s)>127]
+print(beyond_ascii)
+
+#example 2-3 列表推导计算笛卡尔积
+colors = ['black','white']
+sizes =['S','M','L']
+tshirts = [(color,size) for color in colors for size in sizes]
+print(tshirts)
 
